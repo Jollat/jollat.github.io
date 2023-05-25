6a0d4fab0d00f116861e137aee4f3dbcc5623e08
Mercus <hafer16@mail.ru>
2023-05-25 23:37:23 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jollat.github.io',
+  base: '/jollat.github.io/weather-app',
   plugins: [react()]
 })
